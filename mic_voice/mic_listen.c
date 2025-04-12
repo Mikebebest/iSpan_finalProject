3646069542ce5b29ab3357928d75b7e9aa189de4
mike <mich803040@gmail.com>
2025-04-12 22:06:13 +0800

    建立模組框架

---
+++ mic_voice/mic_listen.c
@@ -0,0 +1,65 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <asm/uaccess.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/ioctl.h>
+
+#define DEVICE_NAME "inmp441_mic"
+
+#define MAGIC_NUM 3 
+#define IOCTL_MIC_START_RECORD _IOR(MAGIC_NUM,0,char *)
+#define IOCTL_MIC_STOP_RECORD _IO(MAGIC_NUM,1,char *)
+static struct cdev mic_cdev;
+static dev_t dev_number;
+static struct class *mic_class;
+//int major;
+
+static int mic_open( struct inode *inode, struct file *file){
+    
+    printk(KERN_ALERT "inmp441 麥克風準備收音!\n");
+    return 0;
+}
+
+static int mic_release( struct inode *inode, struct file *file){
+
+    printk(KERN_ALERT "inmp441 麥克風關閉!\n");
+    return 0;
+}
+
+static struct file_operations fops = {
+    .open = mic_open,
+    .unlock_ioctl = mic_ioctl,
+    .release = mic_release
+};
+
+static int __init mic_init(void){
+
+    alloc_chrdev_region(&dev_number,0,1, DEVICE_NAME);
+    
+    cdev_init(&mic_cdev,&fops);
+    mic_cdev.owner = THIS_MODULE;
+    cdev_add(&mic_cdev, dev_number, 1);
+
+    mic_class = class_create(THIS_MODULE, DEVICE_NAME);
+    device_create(mic_class, NULL, dev_number, NULL, DEVICE_NAME);
+
+    printk("register inmp441 mic!\n");
+    return 0;
+}
+
+static void __exit mic_exit(void){
+    
+    device_destroy(mic_class, dev_number);
+    class_destroy(mic_class);
+    cdev_del(&mic_cdev);
+    unregister_chredv_region(dev_number, 1);
+
+    printk("unregister inmp441 mic!\n");
+}
+
+module_init(mic_init);
+module_exit(mic_exit);
+MODULE_LICENSE("GPL");
