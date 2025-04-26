a13e5e4a0277e6e7637005cea97cefbff84a1de2
mike <mich803040@gmail.com>
2025-04-26 15:05:47 +0800

    最終header

--- mic_voice/micdev.h
+++ mic_voice/micdev.h
@@ -2,7 +2,9 @@
 #define MICDEV_H
 #include <linux/ioctl.h>
 
-#define MAGIC_NUM 3 
-#define IOC_MIC_START_RECORD _IOR(MAGIC_NUM,0,char *)
-#define IOC_MIC_STOP_RECORD _IO(MAGIC_NUM,1,char *)
+#define MAGIC_NUM 'm'
+#define IOC_MIC_START_RECORD _IOW(MAGIC_NUM,0,int)
+#define IOC_MIC_STOP_RECORD _IOW(MAGIC_NUM,1,int)
+
+#define MICDEV_VERSION 0222
 #endif
